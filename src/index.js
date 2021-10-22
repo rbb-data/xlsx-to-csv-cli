184d62f5988ed6693300c5dffaaae13589e2b61f
Sophia Mersmann <sophia.mersmann1@gmail.com>
2021-10-22 13:02:11 +0200

    Read filename from command line

---
+++ src/index.js
@@ -0,0 +1,26 @@
+const inquirer = require("inquirer");
+
+inquirer.registerPrompt("fuzzypath", require("inquirer-fuzzy-path"));
+
+const handleError = (error) => {
+  if (error.isTtyError) {
+    // Prompt couldn't be rendered in the current environment
+  } else {
+    // Something else went wrong
+  }
+};
+
+inquirer
+  .prompt([
+    {
+      type: "fuzzypath",
+      name: "filename",
+      excludeFilter: (path) => !path.endsWith(".xlsx"),
+      itemType: "any",
+      message: "Select file",
+    },
+  ])
+  .then((answers) => {
+    console.log(answers);
+  })
+  .catch(handleError);
